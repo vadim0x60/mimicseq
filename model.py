b4871b10ed7c18db6df65b4e623bbc25cce28b69
Vadim Liventsev <dev@vadim.me>
2023-06-06 22:49:43 +0200

    fixed tensor device

--- model.py
+++ model.py
@@ -84,7 +84,7 @@ class TimeSeriesTransformer(L.LightningModule):
     def predict(self, events, intensities):
         embeddings = self.embed(events)
         embeddings *= intensities.nan_to_num(1)
-        embeddings = torch.vstack((embeddings, self.embed(MASK_TOKEN)))
+        embeddings = torch.vstack((embeddings, self.embed(MASK_TOKEN.to(events))))
         embeddings_pred = self(events, intensities)
         return self.unembed(embeddings_pred[:,-1])
     
